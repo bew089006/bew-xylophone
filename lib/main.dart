1ff9385a614d55b7343e985dd0a151fb4edae9ad
wattanan <bew08900@gmail.com>
2020-12-06 10:53:32 +0700

    initail lib and test app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  play_audio() async {
+    player.play('note2.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                play_audio();
+              },
+              child: Text('Press me!'),
+            ),
+          ),
         ),
       ),
     );
