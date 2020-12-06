e085b459b15327530aa31de227bcd63a82269a24
wattanan <bew08900@gmail.com>
2020-12-06 12:32:32 +0700

    dompleted the app

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,19 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   final player = AudioCache();
 
-  play_audio() async {
-    player.play('note2.wav');
+  void playAudio(index) async {
+    player.play('note$index.wav');
+  }
+
+  Expanded buttonAudio(index, color) {
+    return Expanded(
+      child: RaisedButton(
+        onPressed: () {
+          playAudio(index);
+        },
+        color: color,
+      ),
+    );
   }
 
   @override
@@ -15,13 +26,17 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                play_audio();
-              },
-              child: Text('Press me!'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buttonAudio(1, Colors.red),
+              buttonAudio(2, Colors.orange),
+              buttonAudio(3, Colors.yellow),
+              buttonAudio(4, Colors.green),
+              buttonAudio(5, Colors.blueAccent),
+              buttonAudio(6, Colors.lightBlueAccent),
+              buttonAudio(7, Colors.purple),
+            ],
           ),
         ),
       ),
